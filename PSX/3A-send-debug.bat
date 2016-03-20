b4396ac2701151dfbc66f117bd8afa769c05a37d
cyrusdevx <cyrusmaintea@gmail.com>
2016-03-20 14:43:53 -0400

    edits

--- PSX/3A-send-debug.bat
+++ PSX/3A-send-debug.bat
@@ -1,3 +1,3 @@
-java -jar C:\psx\psyq\bin\JavaPSXSend.jar .\bin\PSX.exe COM1 --debug
+java -jar C:\psx\psyq\bin\JavaPSXSend.jar .\bin\PSX.exe COM2 --debug
 
 
--- PSX/3B-send.bat
+++ PSX/3B-send.bat
@@ -1,4 +1,4 @@
 set PATH=C:\Psx\PSYQ\BIN
 
-PSXSERIAL.EXE .\bin\PSX.exe COM1
+PSXSERIAL.EXE .\bin\PSX.exe COM2
 
